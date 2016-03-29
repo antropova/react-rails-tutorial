0455faf8d22c48cfc0903c64127ab6e3630b6028
github username <masha.antropova@gmail.com>
2016-03-29 00:09:15 -0400

    Add components and view

---
+++ app/assets/javascripts/main.jsx
@@ -0,0 +1,16 @@
+class Main extends React.Component {
+    render() {
+        return (
+            <h1>Important content for SEO here...</h1>
+        );
+    };
+};
+
+// let documentReady = () => {
+//     ReactDOM.render(
+//         <Main />,
+//         document.getElementById('react')
+//     );
+// };
+
+// $(documentReady);
